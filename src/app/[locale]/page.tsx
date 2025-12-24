13062f74eff5ce43c812d53984c433c0990c2ac2
christian <christiaanmillaan1313@gmail.com>
2025-12-24 19:35:08 +0100

    enhance link styles with transition effects and improve hover animations

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
   const { data: { user } } = await supabase.auth.getUser()
   const isLoggedIn = !!user
 
-  const linkStyle = "text-xl flex gap-2 items-center justify-center px-5 py-3 rounded-full cursor-pointer"
+  const linkStyle = "text-xl flex gap-2 items-center justify-center px-5 py-3 rounded-full cursor-pointer transition-transform duration-70 ease-out hover:scale-115 active:scale-100"
 
   return (
     <main className = "pb-20 flex min-h-screen flex-col items-center justify-start pt-25 cursor-default select-none bg-[#e3e3e1] text-[#121212] dark:bg-[#121212] dark:text-[#e3e3e1]">
@@ -36,12 +36,12 @@ export default async function Home() {
         <Link
           href = "https://github.com/christian-ms13/frame-guesser"
           target = "_blank"
-          className = {`${linkStyle} font-googlesanscode-light border-2 border-[#121212] dark:border-[#e3e3e1] relative overflow-hidden group transition-all duration-350`}
+          className = {`${linkStyle} font-googlesanscode-light border-2 border-[#121212] dark:border-[#e3e3e1] relative overflow-hidden group`}
         >
           <div className = "absolute inset-0 bg-[url('/home-page/code-bg.gif')] bg-cover bg-center opacity-0 group-hover:opacity-100 transition-opacity duration-350 z-0" />
           <div className = "absolute inset-0 bg-black/47 opacity-0 group-hover:opacity-100 transition-opacity duration-350 z-0" />
-          <IconCode className = "w-6 h-6 relative z-10 group-hover:text-[#e3e3e1]" />
-          <span className = "relative z-10 group-hover:text-[#e3e3e1]">GitHub</span>
+          <IconCode className = "w-6 h-6 relative z-10 group-hover:text-[#e3e3e1] duration-50" />
+          <span className = "relative z-10 group-hover:text-[#e3e3e1] duration-50">GitHub</span>
         </Link>
 
         <Link
